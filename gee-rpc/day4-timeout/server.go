e64f9f442c40783ab3e27fa9834aa07ffee05252
yz <zay85840zhangy@163.com>
2024-07-19 09:44:21 +0800

    'sc'

--- gee-rpc/day4-timeout/server.go
+++ gee-rpc/day4-timeout/server.go
@@ -164,32 +164,37 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interfa
 
 func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup, timeout time.Duration) {
 	defer wg.Done()
-	called := make(chan struct{})
-	sent := make(chan struct{})
+	callTimeOut := make(chan struct{})
+	callFinish := make(chan struct{})
+	sendFinish := make(chan struct{})
+
 	go func() {
 		err := req.svc.call(req.mtype, req.argv, req.replyv)
-		called <- struct{}{}
-		if err != nil {
-			req.h.Error = err.Error()
-			server.sendResponse(cc, req.h, invalidRequest, sending)
-			sent <- struct{}{}
-			return
+		// avoid goroutines may be leaked
+		// make sure that the goroutine is closed even if the function call is timeout
+		select {
+		case <-callTimeOut:
+			close(callFinish)
+			close(sendFinish)
+		case callFinish <- struct{}{}:
+			if err != nil {
+				req.h.Error = err.Error()
+				server.sendResponse(cc, req.h, invalidRequest, sending)
+				sendFinish <- struct{}{}
+				return
+			}
+			server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+			sendFinish <- struct{}{}
 		}
-		server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
-		sent <- struct{}{}
 	}()
 
-	if timeout == 0 {
-		<-called
-		<-sent
-		return
-	}
 	select {
 	case <-time.After(timeout):
+		callTimeOut <- struct{}{}
 		req.h.Error = fmt.Sprintf("rpc server: request handle timeout: expect within %s", timeout)
 		server.sendResponse(cc, req.h, invalidRequest, sending)
-	case <-called:
-		<-sent
+	case <-callFinish:
+		<-sendFinish
 	}
 }
 
@@ -212,10 +217,10 @@ func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
 
 // Register publishes in the server the set of methods of the
 // receiver value that satisfy the following conditions:
-//	- exported method of exported type
-//	- two arguments, both of exported type
-//	- the second argument is a pointer
-//	- one return value, of type error
+//   - exported method of exported type
+//   - two arguments, both of exported type
+//   - the second argument is a pointer
+//   - one return value, of type error
 func (server *Server) Register(rcvr interface{}) error {
 	s := newService(rcvr)
 	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
